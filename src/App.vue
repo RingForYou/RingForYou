aa19a5ca72d56c5b86475f4354e704bd94883202
liheng <1435803646@qq.com>
2021-03-26 09:32:26 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -30,37 +30,26 @@
         enableHtml: false,
         fullStyle: [
           `/*
-* Inspired by http://strml.net/
-* 源码地址 https://github.com/sitexa/anires
-* 大家好，我是南方。
-* 我来写一份简历！
+* 大家好，我是李恒
+* 我来写一份动态简历！
 */
 
-/* 给所有元素加上过渡效果 */
+/* 首先给所有元素加上过渡效果 */
 * {
   transition: all .1s;
 }
-/* 设置背景颜色 */
+/* 白色背景太单调了，我们来点背景 */
 html {
-  color: rgb(222,222,222); background: rgb(0,64,64);
-}
-#content{
-  height:70vh;
-  margin:0;
-}
-#foot{
-  height:29vh;
-  margin:0;
+  color: rgb(222,222,222); background: rgb(0,43,54);
 }
 
-/* 设置边框 */
+/* 文字离边框太近了 */
 .styleEditor {
   padding: .5em;
   border: 1px solid;
   margin: .5em;
   overflow: auto;
-  width: 50vw; height: 70vh;
-  background: rgb(20,20,20);
+  width: 45vw; height: 90vh;
 }
 /* 代码高亮 */
 .token.selector{ color: rgb(130,150,0); }
@@ -80,18 +69,15 @@ html{
           transform: rotateY(10deg) translateZ(-100px) ;
 }
 
-/* 准备一个编辑器 */
+/* 接下来我给自己准备一个编辑器 */
 .resumeEditor{
   position: fixed; right: 0; top: 0;
   padding: .5em;  margin: .5em;
-  width: 50vw; height: 70vh;
+  width: 48vw; height: 90vh;
   border: 1px solid;
-  background: rgb(200,200,200); color: #222;
+  background: white; color: #222;
   overflow: auto;
-  -webkit-transition: none;
-  transition: none;
-  -webkit-transform: rotateY(-10deg) translateZ(-100px) ;
-          transform: rotateY(-10deg) translateZ(-100px) ;
+
 }
 /* 开始写简历 */
 `, `
@@ -126,173 +112,113 @@ html{
   padding: .5em;
   background: #ddd;
 }
-`, `/* 写封感谢信。
- * 感谢大家对我的关注。
- */
-.styleEditor{
-    width:50vw;height:70vh;
-}
-
-.resumeEditor{
-   width:50vw;height:70vh;
-}
-
-.thankEditor{
-  position: relative; left: 0; top: 0;
-  background: #E9D9BB;
-  color: #001C42;
-  overflow: auto;
-}
-
-.thankEditor {
-  width: 99vw; height: 45vh;
-  border: 1px solid #ccc;
-  font-size: .9em;
-}
-`,`
-.thankEditor{
-  padding: .5em;  margin: .5em; margin-top:1em;
-}
-
-.thankEditor ul,.thankEditor ol{
-  list-style: none;
-}
-.thankEditor ul> li::before{
-  content: '☞'; color: red;
-  margin-right: .5em;
-}
-.thankEditor ol {
-  counter-reset: section;
-}
-.thankEditor ol li::before {
-  counter-increment: section;
-  content: counters(section, "☞") " ";
-  margin-right: .5em;
-}
-
-.thankEditor{
-  width: 99vw; height: 45vh;
-  -webkit-transition: none;
-  transition: none;
-  -webkit-transform: rotateX(-10deg) translateZ(-200px);
-          transform: rotateX(-10deg) translateZ(-200px);
-}
 
 `],
         currentMarkdown: '',
         currentThankMarkdown: '',
-        fullMarkdown: `南方
-====
+        fullMarkdown: `李恒
+----
 
-坐标：湖南长沙。
-
-资深软件工程师，资深产品经理，高级项目经理。现为自由职业。
+JAVA高级开发工程师 。
 
 技能
-====
-
-数据库设计
 ----
-  - 结构化数据库设计
-  - noSQL设计
+熟悉 Spring、SpringMVC、MyBatis, hibernate,Spring boot 框架的使用。
+熟练使用 SVN、Git ,Maven等常用开发管理工具。
+熟练使用 Eclipse、Myeclipse、IntellijIDEA，VS2018 等开发工具。
+熟悉Mysql、Oracle 等主流数据库和 redis 缓存数据库。
+熟悉使用操作 linux 操作系统及常用命令；
+熟练使用 docker 搭建开发环境；
+熟悉基于ActiveMQ 的消息分发与接收。
+熟悉 Shiro 技术。
+熟悉RestFul 风格接口设计。
+熟悉 Zookeeper、Dubbo 分布式服务。
+熟悉 Quartz 作业调度框架的使用。
+熟悉C#，有 WPF 的开发经验。
+熟练使用第三方工具如： 微信小程序后台支付企业付款开发， 短信平台等；
+了解 WebService , struts2技术
+能够独立完成项目；
+具有良好的编程风格；
 
-后端开发
-----
-  - 用户管理
-  - 单点登录
-  - 第三方登录
-  - 权限管理
-  - 交易系统
-  - 支付系统
-  - 社区系统
-  - 博客系统
-  - 公众号开发
-  - 小程序开发
-  - API接口开发
-
-前端开发
-----
-  - Web前端开发
-  - 移动终端(Native App, Hybrid App)
-
-产品设计
-----
-  - 智慧旅游项目
-  - 运动健康云平台
-  - 社区支持农业O2O项目
-  - 省级环境监控平台
-  - 高速公路异地处罚系统
-  - 环保局办公自动化系统
-  - 保险公司数据迁移项目
-  - 啤酒厂供应链项目
-  - 货运代理系统
-  - 集装箱管理系统
-  - 滞期费管理项目
-
-技术及语言
-----
-  - Java: SpringMVC, SpringCloud, Hibernate, iBatis, spark, sql2o, HikariCP, freemarker, okHttp, retrofit, RxJava
-  - Kotlin: ktor, exposed, anko
-  - Node.js: express, angular, ionic, react, cordova, meteor, electron, axios
-  - Swift: Vapor, ReactiveSwift
-  - Golang: hugo, beego, gorm, sqlx, matcha
-  - Python: tushare, pandas, numpy, matplotlib
-  - DotNet and PHP
-  - DB: SQLServer, Oracle, MySQL/MariaDB, MongoDB, graphQL, redis, memcached
-  - WebServer: apache, nginx, tomcat, netty, jetty
-  - OS: Ubuntu, CentOS, MacOS, Windows
-  - Others: Docker, git, Xmind，Axure
 
 工作经历
 ----
 
-1. 湖南融耀健康管理有限公司
-2. 湖南三英特旅游智能技术有限公司
-3. 合肥蓝盾科技有限公司
-4. 上海易保网络有限公司
-5. 厦门海环计算机软件有限公司
+工作名称：郑州飞道科技有限公司
+职位名称：Java 开发工程师/C#开发工程师
+工作时间：2018.11-2019.06
+工作职责：
+1. 参与项目需求分析,概要设计和详细设计；
+2. 与项目经理和小组成员沟通协商解决分析开发过程中可能出现的问题；
+3. 对自己的任务模块进行代码设计并进行基础测试；
+工作名称：河南万里科技有限公司
+职位名称：java 开发工程师
+工作时间：2016/8 -- 2018/10
+工作职责：
+1. 负责服务端程序的设计、开发、维护工作
+2. 负责对管理系统的升级，业务的分析和整理
+3. 技术文档编写
+项目经验
+项目名称：真维斯休闲娱乐项目
+项目角色：java工程师
+软件环境：SpringMVC + springboot + mybatis + Maven + redis + MyBatis
+项目描述：电商类网站，该项目是针对休闲娱乐商铺的一个开放入住平台，项目分为商户端，平台端，用户端，类型分
+为休闲娱乐和商务服务，商家入驻平台，内容包括美发，洗浴，运动健身，学习培训等，用户可购买其服务项目，并对
+其评论，分享等。
+项目职责：
+1， 项目采用cookie加redis的方案解决单点登陆问题。用户只需登陆一次，可以访问旗下所有的项目，其他项目
+模块通过cookie+redis获得用户信息，避免用户重复登陆，用户访问其他模块时查询cookie里面对应的KEY值
+去redis中查询用户信息，如果不存在或者过期将会让用户重新登陆；
+2， 商户商务服务端人员技师管理和班次管理模块，商家可以通过该模块添加技师相关信息和班次信息，技师也可
+以根据实际情况预订班次；
+3， 休闲娱乐平台端角色权限管理，用户角色管理和商品管理，用户匿名浏览商品时，可以将商品加入购物车，此
+时的商品信息保存在cookie中，结算时需登陆方可结算，登陆后将cookie中的商品信息拿出同步到该登陆账
+户下的购物车。并已存在的商品数量价格相加。选中商品后可以添加物流地址信息并提交订单。使用quartz定
+时任务框架检测未付款订单，超时两天自动关闭订单，释放库存。
+乐购网
+项目角色：java工程师
+软件环境：Spring+SpringMVC+mybatis+Shiro
+项目描述：适应现金网络发展的需求，公司力求打造一个以手机，电视等电子产品为主的综合性电商超市，乐购电商网
+站专注于电子产品，家用电器市场，持续深耕，为客户提供方便快捷的购物体验。该系统包含门户系统，搜索系统，用
+户系统和后台系统。
+项目职责：
+1. 主要负责基础设置模块和商品展示的生成。
+2. 商品类别的管理以及商品的上下架。
+3. 用户注册、登录功能的实现。
+4. 用户搜索商品，查看商品。
+5. 下单功能的实现。
+项目名称：OA办公
+项目角色：java工程师
+项目描述：利用网络通信技术实现管理自动化和办公事务处理自动化，以提供现代化的日常办公条件，提高办公效率和
+管理水平，旨在为企业、为各个部门提供员工的方便管理。该系统主要针对不同领导层拥有不同的权限级别，管理不同
+自我评价
+部门级别的 员工，比如员工的绩效考核，请假报销审批。该项目主要有员工管理、人员绩 效信息、员工请假、报
+销信息统计等模块。
+项目职责：
+主要参与系统部分模块化的需求分析与详细分析，批量上传人员列表，二级菜单，人员权限分配，接口的设计
+和实现。
+1. 系统后台主要采用ssm框架，MySQL数据库；
+2. 前台页面采用layui，采用页面动态化；
+3. sql优化和tomcat优化作为系统的主要优化；
+4. 采用poi技术实现Excel文件批量上传和下载；
+项目名：思方 ERP 管理系统
+开发环境：VS2018+WPF+MySQL+GIT
+项目描述：共分为货品，财务，销售，工程，生产，报价，设备，品质等模块。对工单、配件、财务等相关数据进行
+全方位管理，规范化、系统化。家电售后服务的所有相关人员。工单快速下单、及时跟进，配件库存实时管理，保障
+售后服务快速及时完成。
+责任描述：
+1. 负责销售模块，报价模块，财务模块及生产模块的代码开发及测试。
+2. 对接相应方案人员，进行需求梳理。
+3. 对新员工的教导培训及总结。 
 
-教育经历
+自我评价
 ----
 
-1. 华东理工大学 环境工程学士
-2. 厦门大学 系统工程硕士
+具有独立开发能力，熟练的专业技能、较强的动手能力，有良好的交际能力和统筹协作能力。能及时反馈并解
+决问题，对工作积极主动、认真负责、善于创新、求知欲强，有较强的学习及适应能力。富有工作激情，乐业敬业，
+具有良好的组织能力，团队协作精神。
 
-文章
-----
-
-* [故土难离（我的父亲母亲）](https://www.meipian.cn/qacqfbz?uuid=d541c15eef694065bc9d1ac9a07925a2)
-* [油腻腻的中年（小诗）](https://www.meipian.cn/wjaz3zh?uuid=ca1cd053b717451da781786de44e66e7)
-* [生命（老四们的蝼蚁人生）](https://www.meipian.cn/vc4pr59?uuid=799c98f5a187405c94c86f7da7788869)
-* [大健康商业模式](http://www.sitexa.org/technology/%E5%A4%A7%E5%81%A5%E5%BA%B7%E5%95%86%E4%B8%9A%E6%A8%A1%E5%BC%8F.html)
-* [社区社交商业模型](http://www.sitexa.org/other/%E7%A4%BE%E5%8C%BA%E7%A4%BE%E4%BA%A4%E5%95%86%E4%B8%9A%E6%A8%A1%E5%9E%8B.html)
-
-链接
-----
-
-* [GitHub](https://github.com/sitexa)
-* [技术博客](http://www.sitexa.org)
-* [南方时代](http://www.sitexa.net)
-* [神秘湘鄂西](http://www.sitexa.cn)
-* [我的简历故事(程序人生公众号文章)](https://mp.weixin.qq.com/s/5rsB956EPVm0FoFOe6xYTg)
-
-勾引方式
-----
-
-* 微信：xnpeng
-
-`, thanksMarkdown: `
-鸣谢
-----
-
-* 这段时间，我有些诚煌诚恐。我的简历被很多人关注(49842），我码的那几篇文章也有很多人点击(20095），微信好友增加了2000多人。"程序人生"公众号文章（我的简历故事）阅读量超过十万+。
-* 有很多企业向我发出了面试邀请，有很多创业团队向我伸出了橄榄枝，一些朋友给我提出非常好的建议，还有很多同学希望跟我进行技术交流，甚至有些企业问我能否承接外包业务。
-* 于是，我受命下山，到了深圳、珠海、北京、天津等地，拜访了一些老板，结识了一些新朋友，见了一些同学和老同事，感触很多，收获很大。IT行业正在发生深刻的变化，新技术、新模式、新思维、新业态，层出不穷，各种探索，各种炒作，让人眼花缭乱。
-* 我对自己也有了一个新的认识。我逐渐发现，我不是去找一份工作，而是在寻找一些机会，一些志同道合的伙伴，一份激动人心的事业。很荣幸，已经有几个朋友跟我达成了意向，准备组建团队。
-* 到底做什么项目呢？我只能简单地说，社交+电商+区块链。肯定有人要说，这个题目太大了，是蹭热度。我不这样想。我以前对社交的思考中，一个重要的因素就是信任关系，而对电商的思考中，去中心化是一个核心考量。而区块链技术不正是解决信任机制和去中心化的利器么？
-* 在此，我欢迎有兴趣的朋友加入我们，尽快推进项目的实施。（微信：xnpeng）
-
-  `
+`
       }
     },
     created() {
